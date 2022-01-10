de097a142d3c96645b034adf1370ceaf296483ba
Ah179 <85572438+Ah179@users.noreply.github.com>
2022-01-10 10:46:25 -0500

    Login.js & Login.css
    Edited App.js to create a separate Login page -->  Login.js & Login.css

--- src/main/ui/frontend/src/App.js
+++ src/main/ui/frontend/src/App.js
@@ -6,8 +6,7 @@ import profile from './Images/brightform.png'
 
 function App() {
   return (
-    <Auth0ProviderWithHistory>
-    {/* <div className="App">
+    { <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,26 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div> */}
-    <div className='login-main'>
-      <div className='sub-login'>
-        <div>
-          <div className='imgs'>
-            <div className='container-img'>
-                <img src = {profile} alt ='profile'></img>
-                //The profile image is NOT IT, 
-                  Looking like a whole mess 
-            </div>
-          </div>
-            <h1>Login Page</h1>
-            <div>
-            //Insert more images, EMPLOYEE ID, PASSWORD
-            </div>
-        </div>
-      </div>
-    </div>
-    <LoginButton />
-    </Auth0ProviderWithHistory>
+    </div> }
   );
 }
 
---
+++ src/main/ui/frontend/src/components/Login/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Auth0ProviderWithHistory from './auth/auth0Provider';
+import LoginButton from './components/Login/LoginButton';
+import profile from './Images/brightform.png'
+
+const Login = () => {
+    return (
+    <Auth0ProviderWithHistory>
+    <div className='login-main'>
+      <div className='sub-login'>
+        <div>
+          <div className='imgs'>
+            <div className='container-img'>
+                <img src = {profile} alt ='profile'></img>
+                //The profile image is NOT IT, 
+                  Looking like a whole mess 
+            </div>
+          </div>
+            <h1>Login Page</h1>
+            <div>
+            //Insert more images, EMPLOYEE ID, PASSWORD
+            </div>
+                <LoginButton />
+        </div>
+      </div>
+    </div>
+    </Auth0ProviderWithHistory>
+    );
+};
+
+export default Login;

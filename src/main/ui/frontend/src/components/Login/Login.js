3db1439c5614b21d510c1ebc30125a6d96ce97b1
Ah179 <85572438+Ah179@users.noreply.github.com>
2022-01-10 13:50:04 -0500

    Login.js

--- src/main/ui/frontend/src/components/Login/Login.js
+++ src/main/ui/frontend/src/components/Login/Login.js
@@ -6,24 +6,21 @@ import profile from '../Images/brightform.png'
 const Login = () => {
     return (
     <Auth0ProviderWithHistory>
-    <div className='login-main'>
+     <div className='login-main'>
       <div className='sub-login'>
         <div>
           <div className='imgs'>
             <div className='container-img'>
                 <img src = {profile} alt ='profile'></img>
-                //The profile image is NOT IT, 
-                  Looking like a whole mess 
             </div>
           </div>
             <h1>Login Page</h1>
             <div>
-            //Insert more images, EMPLOYEE ID, PASSWORD
             </div>
                 <LoginButton />
         </div>
       </div>
-    </div>
+    </div> 
     </Auth0ProviderWithHistory>
     );
 };

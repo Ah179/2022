6d6451ac8781b5ac373220def0de7c63a5cf1123
Ah179 <85572438+Ah179@users.noreply.github.com>
2022-01-10 18:56:04 -0500

    Login Updates,
    Login confirms if input is email or not

--- src/main/ui/frontend/src/components/Login/Login.js
+++ src/main/ui/frontend/src/components/Login/Login.js
@@ -1,32 +1,108 @@
 import React from "react";
+import {Component} from 'react';
 import './Login.css'
 import Auth0ProviderWithHistory from '../../auth/auth0Provider';
-import LoginButton from './LoginButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  FormText,
+  Input,
+  Label,
+  Navbar
+} from 'reactstrap'; 
 import profile from '../Images/brightform.png'
 
-const Login = () => {
-    return (
+//const Login = () =>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      validate: {
+        emailState: '',
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+  } 
+  handleChange = (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+  validateEmail(e) {
+    const emailRex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const { validate } = this.state;
+
+    if (emailRex.test(e.target.value)) {
+      validate.emailState = 'has-success';
+    } else {
+      validate.emailState = 'has-danger';
+    }
+
+    this.setState({ validate });
+  }
+  //THIS PORTION SHOULD SEND BACK INFO TO DATA BASE
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${this.state.email}`);
+  }
+  render() {
+    const {email, password} = this.state;
+    return (  
     <Auth0ProviderWithHistory>
-     <div className='main'>
-      <div className='sub-login'>
+      {/* <Navbar fixed = "top">
         <div>
-          <div className='imgs'>
-            <div className='container-img'>
-                <img src = {profile} alt ='profile'></img>
-            </div>
-            </div>
-            <div>
-              <h1>Login Page</h1>
-              <div>
-                <input type='text' placeholder="Employee ID" className="ID"></input>
-              </div>
-            </div>
-                <LoginButton />
-        </div>
-      </div>
-    </div> 
+          <Container>
+            <Navbar.Brand className='BrightForm'>
+              <img 
+                alt =''
+                src = 'profile'
+                width= '30'
+                height='30'
+                />
+            </Navbar.Brand>
+          </Container>
+          </div>
+        </Navbar> */}
+
+      <div className="LoginPage">
+        <h2>Sign In</h2>
+        <Form className="form">
+          <FormGroup>
+            <Label for="exampleEmail">Employee ID</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="example@example.com"
+            />
+            <FormText>Your Employee ID is your Employee Email.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="********"
+            />
+          </FormGroup>
+        <Button>LOGIN</Button>
+      </Form>
+    </div>
     </Auth0ProviderWithHistory>
     );
+  }
 };
 
 export default Login;

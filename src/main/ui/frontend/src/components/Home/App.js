2e2913682ff44feaa52b01902fd6c52431566dd6
rkapadi <ramsha.kapadia@gmail.com>
2022-01-12 16:38:45 -0500

    Added all the home page code
    I created a Home directory in 2022/src/main/ui/frontend/src/components, which has all the code I've worked on for the main dashboard page.

---
+++ src/main/ui/frontend/src/components/Home/App.js
@@ -0,0 +1,86 @@
+import "@progress/kendo-theme-material/dist/all.css";
+import { TileLayout } from "@progress/kendo-react-layout";
+import { useState } from "react";
+import "./App.css";
+import ProjectOne from "./components/Widgets/ProjectOne";
+import ProjectThree from "./components/Widgets/ProjectThree";
+import ProjectFour from "./components/Widgets/ProjectFour";
+import ProjectTwo from "./components/Widgets/ProjectTwo";
+import Navbar from "./components/Navbar/Navbar";
+
+const initialPositions = [
+  {
+    col: 1,
+    colSpan: 1,
+    rowSpan: 1,
+  },
+  {
+    col: 3,
+    colSpan: 1,
+    rowSpan: 1,
+  },
+  {
+    col: 4,
+    colSpan: 1,
+    rowSpan: 1,
+  },
+  {
+    col: 3,
+    colSpan: 1,
+    rowSpan: 1,
+  },
+];
+
+const getPositions = initialPositions => {
+  // Try to get positions from local storage
+  // If we have none in the storage then default to initial positions
+  return (
+    JSON.parse(localStorage.getItem("dashboard-positions")) || initialPositions
+  );
+};
+
+function App() {
+  const [positions, setPositions] = useState(getPositions(initialPositions));
+
+  const widgets = [
+    {
+      header: "Project Three",
+      body: <ProjectThree />,
+    },
+    {
+      header: "Project One",
+      body: <ProjectOne />,
+    },
+    {
+      header: "Project Two",
+      body: <ProjectTwo />,
+    },
+    {
+      header: "Project Four",
+      body: <ProjectFour />,
+    },
+  ];
+
+  const handleReposition = e => {
+    setPositions(e.value);
+    localStorage.setItem("dashboard-positions", JSON.stringify(e.value));
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Project Dashboard</h1>
+      <TileLayout
+        className="tileLayout"
+        columns={4}
+        rowHeight={255}
+        positions={positions}
+        gap={{ rows: 10, columns: 10 }}
+        items={widgets}
+        onReposition={handleReposition}
+      />
+    </div>
+  );
+}
+
+export default App;

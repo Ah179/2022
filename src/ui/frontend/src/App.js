9a2fd463271a45301f8e23d65d26810f1416409a
Ah179 <85572438+Ah179@users.noreply.github.com>
2022-01-13 12:22:20 -0500

    Merge branch 'main' into ah_frontend

--- src/ui/frontend/src/App.js
+++ src/ui/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import App from './components/Home/App.js';
 import CreateAccount from './components/Login/CreateAccount';
 
 
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path= "/CreateAccount">
             <CreateAccount />
+          </Route>
+          <Route path="/Home">
+            <App />
           </Route>
         </Switch>
       </div>

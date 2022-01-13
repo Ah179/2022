1cd0343fe4b0c6a3799c631da05e741d7a41c75c
qsameen <qizrasameen@gmail.com>
2022-01-13 11:28:05 -0500

    Copied my code over for the Add Project form

---
+++ src/main/ui/frontend/src/components/AddProject/Collaborators/Collaborator.js
@@ -0,0 +1,28 @@
+import React from "react"
+import '../AddProjectForm.css'
+
+class Collaborator extends React.Component {
+
+	renderCollaborator() {
+		return this.props.collaborator.value
+	}
+
+	deleteCollaborator = () => {
+		this.props.remove(this.props.index)
+	}
+
+	render() {
+		return (
+				<React.Fragment>
+					<div className="list">
+						<td className="listText">{this.renderCollaborator()}</td>
+						<td>
+							<button onClick={this.deleteCollaborator} className="delBtn"> - </button>
+						</td>
+					</div>
+				</React.Fragment>
+		)
+	}
+}
+
+export default Collaborator

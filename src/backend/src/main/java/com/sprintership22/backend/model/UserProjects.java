a3e6eb1146005fb0da8cce24490c7150bb757ae4
RRay6 <74483446+RRay6@users.noreply.github.com>
2022-01-14 12:41:38 -0500

    Added UserProjects

---
+++ src/backend/src/main/java/com/sprintership22/backend/model/UserProjects.java
@@ -0,0 +1,44 @@
+package com.sprintership22.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="userprojects")
+public class UserProjects {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+	
+	private int userID;
+	private int projectID;
+	
+	public UserProjects()
+	{
+		
+	}
+	
+	public int getUserID()
+	{
+		return this.userID;
+	}
+	
+	public void setUserID(int userID)
+	{
+		this.userID = userID;
+	}
+	
+	public int getProjectID()
+	{
+		return this.projectID;
+	}
+	
+	public void setProjectID(int projectID)
+	{
+		this.projectID = projectID;
+	}
+}

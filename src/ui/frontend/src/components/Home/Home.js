87384bc9bd294b9b7e5d6d609b414585d6aa1166
rkapadi <ramsha.kapadia@gmail.com>
2022-01-14 12:59:10 -0500

    Editing the Home Folder
    Changed the design of the main dashboard page.

--- src/ui/frontend/src/components/Home/Home.js
+++ src/ui/frontend/src/components/Home/Home.js
@@ -1,88 +1,25 @@
-// import "@progress/kendo-theme-material/dist/all.css";
-// import { TileLayout } from "@progress/kendo-react-layout";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import "@progress/kendo-theme-material/dist/all.css";
 import { useState } from "react";
 import "./Home.css";
-import ProjectOne from "./components/Widgets/ProjectOne";
-import ProjectThree from "./components/Widgets/ProjectThree";
-import ProjectFour from "./components/Widgets/ProjectFour";
-import ProjectTwo from "./components/Widgets/ProjectTwo";
 import Navbar from "./components/Navbar/Navbar";
-import React from "react";
+import Button from 'react-bootstrap/Button';
 
-const initialPositions = [
-  {
-    col: 1,
-    colSpan: 1,
-    rowSpan: 1,
-  },
-  {
-    col: 3,
-    colSpan: 1,
-    rowSpan: 1,
-  },
-  {
-    col: 4,
-    colSpan: 1,
-    rowSpan: 1,
-  },
-  {
-    col: 3,
-    colSpan: 1,
-    rowSpan: 1,
-  },
-];
-
-const getPositions = initialPositions => {
-  // Try to get positions from local storage
-  // If we have none in the storage then default to initial positions
-  return (
-    JSON.parse(localStorage.getItem("dashboard-positions")) || initialPositions
-  );
-};
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Card from './components/Cards/Cards'
 
 function Home() {
-  const [positions, setPositions] = useState(getPositions(initialPositions));
-
-  const widgets = [
-    {
-      header: "Project Three",
-      body: <ProjectThree />,
-    },
-    {
-      header: "Project One",
-      body: <ProjectOne />,
-    },
-    {
-      header: "Project Two",
-      body: <ProjectTwo />,
-    },
-    {
-      header: "Project Four",
-      body: <ProjectFour />,
-    },
-  ];
-
-  const handleReposition = e => {
-    setPositions(e.value);
-    localStorage.setItem("dashboard-positions", JSON.stringify(e.value));
-  };
-
   return (
-    <div className="App">
+    <div className="Home">
       <Navbar />
-      <h1>Project Dashboard</h1>
-      {/* <TileLayout
-        className="tileLayout"
-        columns={4}
-        rowHeight={255}
-        positions={positions}
-        gap={{ rows: 10, columns: 10 }}
-        items={widgets}
-        onReposition={handleReposition}
-      /> */}
+      <h1 className="app-title">Project Dashboard</h1>
+      <Card />
+      <Button variant="outline-dark" onClick={clicked}>Add Project</Button>{' '}
     </div>
   );
+}
+
+function clicked() {
+  alert('You clicked me!');
 }
 
 export default Home;

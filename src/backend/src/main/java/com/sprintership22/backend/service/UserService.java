e04cb11ba2cecbf93d80d78be855b61163a5832a
RRay6 <74483446+RRay6@users.noreply.github.com>
2022-01-18 16:05:09 -0500

    Changes

--- src/backend/src/main/java/com/sprintership22/backend/service/UserService.java
+++ src/backend/src/main/java/com/sprintership22/backend/service/UserService.java
@@ -1,9 +1,14 @@
 package com.sprintership22.backend.service;
 
+import java.util.ArrayList;
+
 import com.sprintership22.backend.model.User;
 
 public interface UserService {
 	public User saveUser(User user);
 	public boolean deleteUser(User user);
 	public boolean verifyUser(User user);
+	public ArrayList<User> emailExists(String email);
+	public ArrayList<User> findUser(String email, String password);
+	public User loginUser(User user);
 }

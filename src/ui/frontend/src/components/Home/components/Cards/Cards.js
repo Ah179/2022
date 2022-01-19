af33570b362b768fe3457d8bbee4b5dbc2f76adc
RRay6 <74483446+RRay6@users.noreply.github.com>
2022-01-19 12:48:05 -0500

    Card formatting

--- src/ui/frontend/src/components/Home/components/Cards/Cards.js
+++ src/ui/frontend/src/components/Home/components/Cards/Cards.js
@@ -29,41 +29,16 @@ function Cards(props)
     return (
         <div className="container-fluid d-flex justify-content-center">
             <div className="row">
-                    {projects.map(project=>(
-                        <div className="col-md-4">
+                {projects.map(project=>(
+                    <div className="col-md-4">
                         <Card title={project.name}
                         dur={project.startTime + " - " + project.endTime}
                         var="working on it"
                         now={100}
-						setUpdateProjectPopupBtn = {props.setUpdateProjectPopupBtn}
-                        />
-                        </div>
-                        ))
-                    }
-                    {/*
-                        <Card title="Project 1"
-                            dur="Project 1 duration goes here"
-                            var="success"
-                            now={100}
-                            setUpdateProjectPopupBtn = {props.setUpdateProjectPopupBtn}
+					    setUpdateProjectPopupBtn = {props.setUpdateProjectPopupBtn}
                         />
                     </div>
-                    <div className="col-md-4">
-                        <Card title="Project 2"
-                            dur="Project 2 duration goes here"
-                            var="warning"
-                            now={60}
-                            setUpdateProjectPopupBtn = {props.setUpdateProjectPopupBtn}
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <Card title="Project 3"
-                            dur="Project 3 duration goes here"
-                            var="warning"
-                            now={20}
-                            setUpdateProjectPopupBtn = {props.setUpdateProjectPopupBtn}
-                        />
-                    </div>*/}
+                ))}
             </div>
         </div>
     )
